9f79cb87fe5cc2c69cdbf3aaf664b655c987795f
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-08-25 18:08:14 +0530

    Add S_element.cpp to calculate sum of vector elements

---
+++ S_element.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    int sum = accumulate(v.begin(), v.end(), 0);
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
